9162c9460db5823817133afef3eb5bc878d437e4
zesu <tannvth2205015@fpt.edu.vn>
2022-11-11 16:00:03 +0700

    ok
    ok

--- js/app.js
+++ js/app.js
@@ -22,6 +22,11 @@ app.config(function($routeProvider) {
             controller: "ContactController"
         })
         // Thông tin về menu chính 
+        .when("/Detailsp", {
+            templateUrl: "spDetails.htm",
+            controller: "DetailController"
+        })
+        // Thông tin về menu chính 
 })
 
 //const menuData =
@@ -125,17 +130,140 @@ app.controller('main_controller', function($scope) {
 
 // --- Thực hiện đối với sản phẩm ,Request + response  từng sản phẩm chi tiết
 
+app.controller('Men1Controller', function($scope, $http) {
+    $scope.users = [
+        { name: "abc", email: "abc@gmail.com", fullname: "abc abc" },
+        { name: "def", email: "def@gmail.com", fullname: "def def" },
+        { name: "ghi", email: "ghi@gmail.com", fullname: "ghi ghi" }
+    ];
+    $scope.DataProdut = [{
+            "Id": 00001,
+            "NameSP": "Hamburg Suites",
+            "Address": "An Der Alster 82",
+            "Description": "Only a few hundred meters from the city center, enjoy the energy of Hamburg each and every night of your stay in our hotel.",
+            "Content": "Nhập nội dung chi tiết vào đâu giới thiệu về sản phẩm",
+            "Discount": 10,
+            "Price": 299,
+            "Num": 55,
+            "size": [{
+                "NoSiz": 33,
+                "NoSiz": 34,
+                "NoSiz": 35,
+                "NoSiz": 36
+            }],
+            "color ": "vàng",
+            "Images ": [{
+                "FileName": "250x250.png ",
+            }, {
+                "FileName": "400x600.png",
+            }, {
+                "FileName": "400x600.png",
+                "widthRatio": 2,
+                "heightRatio": 2
+            }, {
+                "FileName": "1500x1500.png",
+                "widthRatio": 2,
+                "heightRatio": 2
+            }, {
+                "FileName": "Bedroom-1-2.jpg",
+            }, {
+                "FileName": "Bathroom-0.jpg",
+            }]
+        },
+        {
+            "Id": 00001,
+            "NameSP": "Hamburg Suites",
+            "Address": "An Der Alster 82",
+            "Description": "Only a few hundred meters from the city center, enjoy the energy of Hamburg each and every night of your stay in our hotel.",
+            "Content": "Nhập nội dung chi tiết vào đâu giới thiệu về sản phẩm",
+            "Discount": 10,
+            "Price": 299,
+            "Num": 55,
+            "size": [{
+                "NoSiz": 33,
+                "NoSiz": 34,
+                "NoSiz": 35,
+                "NoSiz": 36
+            }],
+            "color ": "vàng",
+            "Images ": [{
+                "FileName": "250x250.png ",
+            }, {
+                "FileName": "400x600.png",
+            }, {
+                "FileName": "400x600.png",
+                "widthRatio": 2,
+                "heightRatio": 2
+            }, {
+                "FileName": "1500x1500.png",
+                "widthRatio": 2,
+                "heightRatio": 2
+            }, {
+                "FileName": "Bedroom-1-2.jpg",
+            }, {
+                "FileName": "Bathroom-0.jpg",
+            }]
+        }, {
+            "Id": 0003,
+            "NameSP": "Sản phẩm quần âu ",
+            "Address": "An Der Alster 82",
+            "Description": "Only a few hundred meters from the city center, enjoy the energy of Hamburg each and every night of your stay in our hotel.",
+            "Content": "Nhập nội dung chi tiết vào đâu giới thiệu về sản phẩm",
+            "Discount": 10,
+            "Price": 5000000,
+            "Num": 55,
+            "size": [{
+                "NoSiz": 33,
+                "NoSiz": 34,
+                "NoSiz": 35,
+                "NoSiz": 36
+            }],
+            "color ": "vàng",
+            "Images ": [{
+                "FileName": "250x250.png ",
+            }, {
+                "FileName": "400x600.png",
+            }, {
+                "FileName": "400x600.png",
+                "widthRatio": 2,
+                "heightRatio": 2
+            }, {
+                "FileName": "1500x1500.png",
+                "widthRatio": 2,
+                "heightRatio": 2
+            }, {
+                "FileName": "Bedroom-1-2.jpg",
+            }, {
+                "FileName": "Bathroom-0.jpg",
+            }]
+        }
+
+    ];
 
 
 
-// app.controller('MenController', function($scope, $http) {
+});
+
+
+
+// app.controller('Men2Controller', function($scope, $http) {
 //     $scope.datas = new Array;
 //     $http.get("datapro.json").then(function(response) {
-//         $scope.datas = JSON.parse(response);
+//         $scope.datas = response;
 //     });
-//     $scope.DataProduct = $scope.datas;
+//     // $scope.DataProduct = $scope.datas;
 // });
 
+app.controller('MenController', function($scope, $http) {
+    // $scope.datas = new Array;
+    fetch('datapro.json')
+        .then(response => response.json())
+        .then(data => console.log('finished'));
+    $scope.Pro = data;
+    // $http.get('datapro.json').success(function(data) {
+
+
+});
 
 
 /*
@@ -200,7 +328,9 @@ app.controller('homeController',
                         const item = data.itemData;
                         if (item.price) {
                             $scope.productName = item.name;
-                            $scope.productPrice = `$${item.price}`;
+                            $scope.productPrice = `
+                        $$ { item.price }
+                        `;
                             $scope.productImage = item.icon;
                         }
                     },
